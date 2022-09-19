8020ee5d0434660a28505eac42a5e4764ebb147d
detta123 <kushagradrdixit@gmail.com>
2022-09-19 18:21:34 +0530

    Updated

--- .vscode/binary_search.cpp
+++ .vscode/binary_search.cpp
@@ -2,7 +2,20 @@
 using namespace std;
 int index(int arr[],int n,int key){
     int s=0;
-    
+    int l=n-1;
+    while(s<l){
+        int mid=(s+l)/2;
+        if(arr[mid]==key){
+            return mid+1;
+        }
+        else if(arr[mid]<key){
+            s=mid+1;
+        }
+        else{
+            l=mid-1;
+        }
+    }
+    return -1;
 }
 int main(){
     int arr[100];
